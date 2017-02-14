38b8883bc79c3bc093cc2b806657f598382b4a67
meltzerpete <meltzerpete@gmail.com>
2017-02-14 10:03:00 +0000

    simple 3-layer MLP set up to learn XOR

---
+++ mlp3/mlp3/Data.java
@@ -0,0 +1,29 @@
+package mlp3;
+
+/**
+ * Created by pete on 05/02/17.
+ */
+public class Data {
+
+    private Data(){};
+
+    /**
+     * Generate XOR data set.
+     * @param numberOfPatterns
+     * @return Array of patterns where first two elements are input and last element is desired output.
+     */
+    public static int[][] getXORData(int numberOfPatterns){
+
+        int patterns[][] = new int[numberOfPatterns][3];
+
+        for (int p = 0; p < numberOfPatterns; p++) {
+            for (int i = 0; i < 2; i++) {
+                patterns[p][i] = Math.random() > 0.5 ? 1 : 0;
+            }
+            patterns[p][2] = (patterns[p][0] + patterns[p][1] == 1) ? 1 : 0;
+        }
+
+        return patterns;
+
+    }
+}
